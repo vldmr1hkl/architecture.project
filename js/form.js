0c59fa65f6b5583a8b8fc147658bbce45f02e6eb
vldmr7hk <vladimir_hkl@outlook.com>
2023-12-11 16:39:53 +0200

    update styles

--- js/form.js
+++ js/form.js
@@ -30,21 +30,14 @@ document.addEventListener("partialsLoaded", () => {
             }
         }
 
-        // Перевірка імені
         const isNameValid = validateField('nameInput', 'nameError', /^[a-zA-Z]+ [a-zA-Z]+$/, 'Please enter a name','Please enter a valid full name, eg "John Carter"');
 
-
-        // Перевірка електронної пошти
         const isEmailValid = validateField('emailInput', 'emailError', /^[^\s@]+@[^\s@]+\.[^\s@]+$/, 'Please fill in the field', 'Please enter a valid email address, eg. "email@example.com');
 
-        // Валідуємо отримане форматоване значення телефонного номеру
-        const isPhoneValid = validateField('phoneInput', 'phoneError', /^\(\d{3}\)-\d{3} \d{4}$/,'Please enter a phone number', 'Please enter a valid phone number, eg. (123)-456 7890');
+        const isPhoneValid = validateField('phoneInput', 'phoneError', /^\d+$/, 'Please enter a phone number', 'Please enter a valid phone number, eg. (123)-456 7890');
 
-
-        // Перевірка теми
         const isSubjectValid = validateField('subjectInput', 'subjectError', /.+/,'Please select a service.');
 
-        // Перевірка повідомлення
         const isMessageValid = validateField('messageInput', 'messageError', /.+/, 'Please enter a message.');
 
         return isNameValid && isEmailValid && isPhoneValid && isSubjectValid && isMessageValid;
